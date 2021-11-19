eeab0af733978a0ba37d85bed7abc5e0af9a03f2
Chrish Dunne <hey@chrish.design>
2021-11-19 12:15:40 +0000

    feat: Added clients list

---
+++ schemas/clients.js
@@ -0,0 +1,12 @@
+export default {
+  name: "clients",
+  title: "Clients",
+  type: "document",
+  fields: [
+    {
+      name: "clientId",
+      title: "Client Identifier",
+      type: "string",
+    },
+  ],
+};
