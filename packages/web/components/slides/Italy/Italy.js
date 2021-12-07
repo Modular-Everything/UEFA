8ed0584dacb8cfee4953b2ff32a19b9a61c3ed12
Chrish Dunne <hey@chrish.design>
2021-12-07 10:34:23 +0000

    feat: Progress on Italy deck slide

--- packages/web/components/slides/Italy/Italy.js
+++ packages/web/components/slides/Italy/Italy.js
@@ -1,13 +1,24 @@
+import { useNextSanityImage } from "next-sanity-image";
+import Image from "next/image";
+
+import { sanityConfig } from "../../../lib/config";
 import { Headline } from "../../elements/Headline";
 import * as S from "./Italy.styles";
 import { Pattern } from "./Pattern";
 
 export function Italy({ data }) {
+  const imageProps = useNextSanityImage(sanityConfig, data?.image);
+
   return (
     <S.Italy>
       <div className="content">
         <Headline data={{ headline: data?.headline }} />
       </div>
+
+      <div className="image">
+        <Image {...imageProps} quality={95} />
+      </div>
+
       <Pattern />
     </S.Italy>
   );
--- packages/web/components/slides/Italy/Italy.styles.js
+++ packages/web/components/slides/Italy/Italy.styles.js
@@ -3,16 +3,6 @@ import styled from "styled-components";
 export const Italy = styled.article`
   height: 100%;
 
-  .content {
-    display: flex;
-    flex-direction: column;
-    padding: var(--content-gutter);
-    position: relative;
-    z-index: 500;
-    height: 100%;
-    width: 100%;
-  }
-
   h2 {
   }
 
@@ -24,5 +14,25 @@ export const Italy = styled.article`
     right: 0;
     width: 100%;
     z-index: 0;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    padding: var(--content-gutter);
+    position: relative;
+    z-index: 500;
+    height: 100%;
+    width: 100%;
+    z-index: 100;
+  }
+
+  .image {
+    position: absolute;
+    z-index: 50;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    width: clamp(50rem, 70vw, 100rem);
   }
 `;
