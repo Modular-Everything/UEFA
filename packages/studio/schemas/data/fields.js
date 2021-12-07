623a5cf7701db22f2a51b626e44501f7ae397667
Chrish Dunne <hey@chrish.design>
2021-12-07 12:10:52 +0000

    feat: Added pills field

--- packages/studio/schemas/data/fields.js
+++ packages/studio/schemas/data/fields.js
@@ -28,3 +28,40 @@ export const image = {
   title: "Slide Image",
   type: "image",
 };
+
+export const pills = {
+  name: "pills",
+  title: "Data Pills",
+  type: "array",
+  of: [
+    {
+      name: "pill",
+      title: "Pill",
+      type: "object",
+      fields: [
+        {
+          name: "data",
+          title: "Data",
+          type: "string",
+          description: 'The number or value, for example "1.8b", "300m", etc.',
+        },
+        {
+          name: "context",
+          title: "Context",
+          type: "string",
+          description:
+            'The context for the data, for example "Facebook interactions"',
+        },
+      ],
+      preview: {
+        select: {
+          data: "data",
+          context: "context",
+        },
+        prepare: ({ data, context }) => ({
+          title: `${data || ""} ${context || ""}`,
+        }),
+      },
+    },
+  ],
+};
