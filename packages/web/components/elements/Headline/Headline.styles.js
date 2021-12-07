aa55a094f76213eace31e0a1aecf70d7b448a086
Chrish Dunne <hey@chrish.design>
2021-12-07 10:58:13 +0000

    feat: Responsive headline text

--- packages/web/components/elements/Headline/Headline.styles.js
+++ packages/web/components/elements/Headline/Headline.styles.js
@@ -2,11 +2,20 @@ import styled from "styled-components";
 
 export const Headline = styled.h2`
   text-transform: uppercase;
-  font-size: clamp(2.4rem, 6vw, 6.4rem);
-  max-width: 42rem;
-  width: 10vw;
-  line-height: clamp(2rem, 5vw, 5.6rem);
+  font-size: clamp(4rem, 6vw, 6.4rem);
+  line-height: clamp(3rem, 5vw, 5.6rem);
   color: var(--uefa-lt-blue);
   border-left: 1px solid var(--uefa-yellow);
   padding: 1.6rem 0;
+  width: 100%100vh;
+
+  @media (min-width: 375px) {
+    width: 50vw;
+    max-width: 32rem;
+  }
+
+  @media (min-width: 1000px) {
+    width: 50vw;
+    max-width: 45rem;
+  }
 `;
