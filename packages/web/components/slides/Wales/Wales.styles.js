67b13f05d82156ad14edfbcf9d54f3732d9e00b8
Chrish Dunne <hey@chrish.design>
2021-12-17 10:52:05 +0000

    feat: Adjusted  some spacing settings

--- packages/web/components/slides/Wales/Wales.styles.js
+++ packages/web/components/slides/Wales/Wales.styles.js
@@ -14,18 +14,24 @@ export const Wales = styled.article`
       flex-direction: column-reverse;
       align-items: center;
       max-width: 88rem;
-      gap: 4.8rem;
+      gap: 2.4rem;
 
       @media (min-width: 768px) {
+        gap: 4.8rem;
         flex-direction: row;
       }
 
       .simpleCopy {
         width: 100%;
+        text-align: left;
 
         @media (min-width: 375px) {
           width: 50vw;
           max-width: 32rem;
+        }
+
+        @media (min-width: 768px) {
+          text-align: right;
         }
       }
     }
