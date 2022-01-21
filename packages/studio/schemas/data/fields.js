208e1fb86752d45f033bddf0b5c695bbe807ae8f
Chrish Dunne <hey@chrish.design>
2022-01-21 16:06:06 +0000

    feat: Added bullets and bars schema

--- packages/studio/schemas/data/fields.js
+++ packages/studio/schemas/data/fields.js
@@ -93,3 +93,104 @@ export const number = {
   title: "Number",
   type: "number",
 };
+
+export const bullets = {
+  name: "bullets",
+  title: "Bullets",
+  type: "array",
+  of: [
+    {
+      name: "bullet",
+      title: "Bullet",
+      type: "object",
+      fields: [
+        {
+          name: "label",
+          title: "Label",
+          type: "string",
+        },
+        {
+          name: "bulletColor",
+          title: "Bullet Color",
+          type: "string",
+          initialValue: "#1e1e28",
+          options: {
+            list: brandColorsList,
+          },
+        },
+      ],
+      preview: {
+        select: {
+          title: "label",
+          color: "bulletColor",
+        },
+        prepare: ({ title, color }) => ({
+          title,
+          media: (
+            <span
+              style={{
+                backgroundColor: color,
+                width: 10,
+                height: 10,
+                borderRadius: 100,
+              }}
+            />
+          ),
+        }),
+      },
+    },
+  ],
+};
+
+export const bars = {
+  name: "bars",
+  title: "Bars",
+  type: "array",
+  of: [
+    {
+      name: "bar",
+      title: "Bar",
+      type: "object",
+      fields: [
+        {
+          name: "label",
+          title: "Label",
+          type: "string",
+        },
+        {
+          name: "value",
+          title: "Value",
+          type: "string",
+        },
+        {
+          name: "barColor",
+          title: "Bar Color",
+          type: "string",
+          initialValue: "#749baa",
+          options: {
+            list: brandColorsList,
+          },
+        },
+      ],
+      preview: {
+        select: {
+          title: "label",
+          color: "barColor",
+        },
+        prepare: ({ title, color }) => ({
+          title,
+          media: (
+            <span
+              style={{
+                backgroundColor: color,
+                width: 30,
+                height: 10,
+                borderRadius: 100,
+              }}
+            />
+          ),
+        }),
+      },
+    },
+  ],
+};
