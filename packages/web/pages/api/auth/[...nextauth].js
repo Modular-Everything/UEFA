4e2853c110db5e89c78a11f72a06a9305133f7bd
Chrish Dunne <hey@chrish.design>
2022-02-16 10:37:07 +0000

    feat: Further work on logins

--- packages/web/pages/api/auth/[...nextauth].js
+++ packages/web/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 /* eslint-disable new-cap */
-/* eslint-disable import/no-default-export */
+
 import NextAuth from "next-auth";
+// import EmailProvider from "next-auth/providers/email";
 import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
@@ -10,6 +11,10 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    // EmailProvider({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    // }),
     // ...add more providers here
   ],
 });
