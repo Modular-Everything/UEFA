7266242c4caf662f44d86df1c94659bd69c03e18
Chrish Dunne <hey@chrish.design>
2022-02-18 12:33:29 +0000

    feat: Tweak content spacing

--- packages/web/components/elements/Content/Content.styles.js
+++ packages/web/components/elements/Content/Content.styles.js
@@ -8,14 +8,14 @@ export const Content = styled.div`
   width: 100%;
   height: 100%;
   margin: 0 auto;
-  padding: calc(var(--content-gutter) * 2) var(--content-gutter)
+  padding: calc(var(--content-gutter) * 2.5) var(--content-gutter)
     var(--content-gutter);
   display: flex;
   flex-direction: column;
   position: relative;
   z-index: 100;
 
-  @media (min-width: 768px) {
+  /* @media (min-width: 768px) {
     padding: var(--content-gutter);
-  }
+  } */
 `;
