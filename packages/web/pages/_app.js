8935b9cd0500dd1cb213d2f27bef0ca7f69d506f
Chrish Dunne <hey@chrish.design>
2022-03-04 08:37:16 +0000

    feat: Added logrocket

--- packages/web/pages/_app.js
+++ packages/web/pages/_app.js
@@ -1,11 +1,15 @@
+import LogRocket from "logrocket";
 import { SessionProvider } from "next-auth/react";
-
 import "../styles/initialize.css";
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Script id="logrocket">
+        {typeof window !== "undefined" && LogRocket.init("rpltij/uefa")}
+      </Script>
       <Component {...pageProps} />
     </SessionProvider>
   );
